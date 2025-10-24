d0797000af33a2649614f63ff8d1b39d21a94175
Lennishkin <60464253+Lennishkin@users.noreply.github.com>
2025-10-24 09:28:21 +0300

    iisisi

---
+++ nextjs-dashboard/app/query/route.ts
@@ -0,0 +1,23 @@
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+async function listInvoices() {
+	const data = await sql`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 666;
+  `;
+
+	return data;
+}
+
+export async function GET() {
+	try {
+		const data = await listInvoices();
+		return new Response(JSON.stringify(data), { headers: { 'content-type': 'application/json' } });
+	} catch (error) {
+		return new Response(JSON.stringify({ error: String(error) }), { status: 500, headers: { 'content-type': 'application/json' } });
+	}
+}
